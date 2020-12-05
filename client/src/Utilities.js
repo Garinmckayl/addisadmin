8bf2a05743695555f0720c73425cbbc77cf7cb29
Natnael Getenew <natnaelgetenew@gmail.com>
2020-12-05 08:28:47 +0000

    redux-saga, readme

---
+++ client/src/Utilities.js
@@ -0,0 +1,31 @@
+///////////////////
+// ActionHelpers //
+///////////////////
+const asyncTypes = {
+  FETCH_ALL: 'FETCH_ALL',
+  CREATE: 'CREATE',
+  UPDATE: 'UPDATE',
+  DELETE: 'DELETE'
+}
+
+
+export const createAsyncTypes = typeString =>
+  Object.values(asyncTypes).reduce((acc, curr) => {
+    acc[curr] = `${typeString}_${curr}`
+    return acc
+  }, {})
+
+
+  export const createAction = 
+  (type, payload = {}) => 
+    ({ type, ...payload })
+
+    ///////////////////
+// createReducer //
+///////////////////
+export const createReducer = 
+  (initialState, handlers) =>
+    (state = initialState, action) =>
+      handlers.hasOwnProperty(action.type)
+      ? handlers[action.type](state, action)
+      : state
--- client/src/reducers/employees.js
+++ client/src/reducers/employees.js
@@ -1,14 +1,16 @@
 // import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { createAsyncTypes } from '../Utilities'
+export const GET_EMPLOYEE_ASYNC = createAsyncTypes('GET_EMPLOYEE')
 
 export default (employees = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case GET_EMPLOYEE_ASYNC.FETCH_ALL:
       return action.payload;
-    case 'CREATE':
+    case GET_EMPLOYEE_ASYNC.CREATE:
       return [...employees, action.payload];
-    case 'UPDATE':
+    case GET_EMPLOYEE_ASYNC.UPDATE:
       return employees.map((employee) => (employee._id === action.payload._id ? action.payload : employee));
-    case 'DELETE':
+    case GET_EMPLOYEE_ASYNC.DELETE:
       return employees.filter((employee) => employee._id !== action.payload);
     default:
       return employees;
